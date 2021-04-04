d582c228d3d35f87a407b5004487af691333a15f
Ma-XD <7721772@mail.ru>
2021-04-04 13:23:30 +0300

    Change float to double because there was error on big data

--- hw5/main.cpp
+++ hw5/main.cpp
@@ -4,10 +4,10 @@
 #include <chrono>
 #include <omp.h>
 
-const float EPS = 1.0e+05;
 int num_threads;
+const double EPS = 1.0e-05;
 
-std::vector<float> read_file(const std::string & file_name) {
+std::vector<double> read_file(const std::string & file_name) {
     std::ifstream fin;
     fin.open(file_name);
     if (!fin.is_open()) {
@@ -15,36 +15,36 @@ std::vector<float> read_file(const std::string & file_name) {
     }
     int size;
     fin >> size;
-    std::vector<float> array(size);
-    for (float & a : array) {
+    std::vector<double> array(size);
+    for (double & a : array) {
         fin >> a;
     }
     fin.close();
     return array;
 }
 
-void write_file(const std::vector<float> & array, const std::string & file_name) {
+void write_file(const std::vector<double> & array, const std::string & file_name) {
     std::ofstream fout;
     fout.open(file_name);
     if (!fout.is_open()) {
         std::cerr << "Cannot open file \"" << file_name << "\"";
     }
     fout << array.size() << "\n";
-    for (float a : array) {
+    for (double a : array) {
         fout << a << " ";
     }
     fout.close();
 }
 
-std::vector<float> not_parallel(const std::vector<float> & array) {
-    std::vector<float> prefix(array.size());
+std::vector<double> not_parallel(const std::vector<double> & array) {
+    std::vector<double> prefix(array.size());
     std::chrono::system_clock::time_point start_time = std::chrono::system_clock::now();
     prefix[0] = array[0];
     for (int i = 1; i < array.size(); i++) {
         prefix[i] = array[i] + prefix[i - 1];
     }
     std::chrono::system_clock::time_point end_time = std::chrono::system_clock::now();
-    std::chrono::duration<float> time = end_time - start_time;
+    std::chrono::duration<double> time = end_time - start_time;
     printf("\nNot parallel time (%i thread(s)): %f ms\n", num_threads, time.count());
     return prefix;
 }
@@ -58,10 +58,10 @@ int get_log2(int n) {
     return res;
 }
 
-std::vector<float> parallel(const std::vector<float> & array) {
+std::vector<double> parallel(const std::vector<double> & array) {
     int n = array.size();
     int logn = get_log2(n);
-    std::vector<float> temp (n * (logn + 1));
+    std::vector<double> temp (n * (logn + 1));
     temp[0] = 0;
 #pragma omp parallel for default(none) shared(temp, array, n)
     for (int i = 1; i < n; i++) {
@@ -80,10 +80,10 @@ std::vector<float> parallel(const std::vector<float> & array) {
         }
     }
     std::chrono::system_clock::time_point end_time = std::chrono::system_clock::now();
-    std::chrono::duration<float> time = end_time - start_time;
+    std::chrono::duration<double> time = end_time - start_time;
     printf("\nParallel time (%i thread(s)): %f ms\n", num_threads, time.count());
 
-    std::vector<float> prefix(array.size());
+    std::vector<double> prefix(array.size());
 #pragma omp parallel for default(none) shared(temp, array, prefix, logn, n)
     for (int i = 0; i < n; ++i) {
         prefix[i] = temp[n * logn + i] + array[i];
@@ -111,7 +111,7 @@ int main(int argc, char *argv[]) {
     omp_set_num_threads(num_threads);
 
     std::string input_file_name = argv[2];
-    std::vector<float> array = read_file(input_file_name), prefix_np, prefix_p;
+    std::vector<double> array = read_file(input_file_name), prefix_np, prefix_p;
     prefix_np = not_parallel(array);
     prefix_p = parallel(array);
     for (int i = 0; i < array.size(); ++i) {
@@ -126,7 +126,7 @@ int main(int argc, char *argv[]) {
         write_file(prefix_p, output_file_name);
     } else {
         std::cout << prefix_p.size() << std::endl;
-        for (float a : prefix_p) {
+        for (double a : prefix_p) {
             std::cout << a << " ";
         }
     }
